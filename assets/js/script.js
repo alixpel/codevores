5fc1fa409777273e3aaa2889e8c30b22d0a3356d
alixpel <alixpelletierpro@gmail.com>
2020-03-29 11:13:07 +0200

    Formulaire de contact de la page codeuse

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,6 +82,27 @@ document.querySelector('.close-button').addEventListener('click', function(){
 });
 
 // ////////////////////////////////////////////////////////////////////
+// Ouverture des coordonnées (page codeuse)
+
+    document.querySelector('.masquer-coordonnees').style.display = "none";
+    document.querySelector('.coordonnees').style.display = 'none';
+
+
+document.querySelector('.afficher-coordonnees').addEventListener('click', function(){
+    document.querySelector('.afficher-coordonnees').style.display = "none";
+    document.querySelector('.masquer-coordonnees').style.display = "block";
+    document.querySelector('.coordonnees').style.display = 'block';
+});
+
+// Fermeture des coordonnées
+
+document.querySelector('.masquer-coordonnees').addEventListener('click', function(){
+    document.querySelector('.afficher-coordonnees').style.display = "block";
+    document.querySelector('.masquer-coordonnees').style.display = "none";
+    document.querySelector('.coordonnees').style.display = 'none';
+});
+
+// ////////////////////////////////////////////////////////////////////
 // animation du texte
 function animated_text1() {
   let delay = 100,
